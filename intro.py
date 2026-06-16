cdf1ead479e3499a1209d42f303f196ef678aa88
LivinkumarStackly <livinkumar@thestackly.com>
2026-06-16 11:19:46 +0530

    Introduction

---
+++ intro.py
@@ -0,0 +1,12 @@
+'''these are printing statements
+these are printing statements
+these are printing statements
+these are printing statements'''
+
+# print(1)
+# print(200)
+# print(50)
+
+print(1)
+print(200)
+print(50)
