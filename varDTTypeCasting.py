c9c48839426e214a9ee6289f497ad413085a2544
LivinkumarStackly <livinkumar@thestackly.com>
2026-06-18 11:13:47 +0530

    Variables, Datatypes and Typecasting

---
+++ varDTTypeCasting.py
@@ -0,0 +1,32 @@
+# a=10
+# a=15
+# pr=3
+
+# print(a)
+# print(a)
+# print(a)
+
+a=-100
+b=1.01
+c="I'm Leo"
+# print(type(a)) #<class 'int'>
+# print(type(b)) 
+
+str1="Hello"
+str2="World"
+
+b1=True
+# print(type(b1))
+
+# print(str1+str2+"....!")
+# print(5+6.11)
+# print("Hello" + 6)
+
+# print(type(c))
+
+name="Leo"
+age=44
+
+# His name is Leo. He is 44.
+
+print("His name is "+name+". He is "+str(age))
