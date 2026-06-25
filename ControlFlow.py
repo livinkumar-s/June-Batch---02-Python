718daa797b89dc05e605ad52d4af1068e58d96ff
LivinkumarStackly <livinkumar@thestackly.com>
2026-06-25 11:48:24 +0530

    Control Flow Statements - Conditional Clause

---
+++ ControlFlow.py
@@ -0,0 +1,69 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# a=int(input("Enter your Number: "))
+# print(a+100)
+# print(a-100)
+
+# age=int(input("Enter Your Age: "))
+# if age>=18:
+#     print("Can Vote")
+# else:
+#     print("Cannot Vote")
+
+# print(6)
+
+# a=10
+
+# if a<10:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+# else:
+#     print("From else block...!")
+
+# print("Step Last")
+
+# a=10
+
+# if a<10:
+#     print("From IF Block...!")
+# elif a<10:
+#     print("From Elif1 Block")
+# elif a>10:
+#     print("From Elif2 Block")
+# else:
+#     print("From else Block...!")
+
+# marks = int(input("Enter your marks: "))
+ 
+# if marks >= 90 and marks <= 100:
+#     print("Grade: O")
+# elif marks >= 80 and marks <= 89:
+#     print("Grade: A+")
+# elif marks >= 70 and marks <= 79:
+#     print("Grade: A")
+# elif marks >= 60 and marks <= 69:
+#     print("Grade: B")
+# elif marks >= 50 and marks <= 59:
+#     print("Grade: C")
+# elif marks >= 0 and marks <= 49:
+#     print("Grade: Fail")
+# else:
+#     print("Invalid Marks")
+
+num=int(input("Enter your Number: "))
+
+if num>10:
+    if num>15:
+        print("Very Large...!")
+    else:
+        print("Large")
+else:
+    if num<5:
+        print("Very Small")
+    else:
+        print("Small")
