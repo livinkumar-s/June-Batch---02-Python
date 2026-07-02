2cb45bf6bd9e51e004f3d6d4ae6f25e8b53663d0
LivinkumarStackly <livinkumar@thestackly.com>
2026-07-02 11:46:00 +0530

    DataStructures - List, Tuple

---
+++ DataStructures.py
@@ -0,0 +1,63 @@
+# List
+
+l1=[1,2,3,5.5,"Hello",False,1]
+# l1[2]=33
+# print(len(l1))
+# l1.append(77)
+# l1.append(78)
+# l1.insert(-3,55)
+# l1.append([1,2,3])
+# l1.extend("Hello")
+# l1.remove(1)
+# l1.remove(1)
+# l1.pop()
+# l1.pop()
+# print(l1.count(99))
+# print(l1.index(66))
+
+# l2=[3,2,4,5,6,7,4]
+# print(l2[1:3]) #[2]
+# l2.reverse()
+# l2.sort(reverse=True)
+# print(l2)
+# print(l2[1])
+
+# print(l2[1:]) 
+# print(l2[2:4]) #2,3
+# print(l2[1:6:2]) #[1,3,5]
+# print(l2[::-1]) #[]
+
+# l3=[
+#     1,
+#     2,
+#     3,
+#     [
+#         "four",
+#         "five",
+#         "six",
+#         [
+#             "seven",
+#             "eight"
+#         ]
+#     ]
+# ]
+
+# print(l3[-1][-1][-1][-1])
+
+# Tuple
+
+# t1=(1,2,3,4,3,2,1,3,3,3,3)
+
+# t1[3]=99
+
+# print(t1)
+
+# Person=["Ken",44,"FED"]
+
+# name,age,role=Person
+
+# name=Person[0]
+# age=Person[1]
+# role=Person[2]
+
+# print(role)
