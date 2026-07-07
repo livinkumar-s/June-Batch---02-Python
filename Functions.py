742968bf99c0a3b6b46cd6bdc9656b0693ec3d00
LivinkumarStackly <livinkumar@thestackly.com>
2026-07-07 12:03:21 +0530

    Functions

---
+++ Functions.py
@@ -0,0 +1,51 @@
+# def printStep():
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     print("---------------------")
+
+# printStep()
+# printStep()
+# printStep()
+
+# def addNum(a,b):
+#     print(a)
+#     print(b)
+#     print(a+b)
+
+# addNum(b=4,a=5)
+# addNum(40,57)
+# addNum(b=45,a=43)
+
+# def addAll(x,y,z=10):
+#     print(x+y+z)
+
+# addAll(12,13,100)
+
+# def mulAll(*a):
+#     ans=1
+#     for i in a:
+#         ans*=i 
+#     print(ans)
+
+# mulAll(1,2,3)
+
+# def getDetails(**person):
+#     print(person)
+
+# getDetails(name="Ken",age=44,role="FED")
+
+
+# def printStep():
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+#     print("---------------------")
+
+# a=printStep()
+# print(a)
+
+# print("Hello")
+
+# a=input("Enter you fav cricketer: ")
+# print(a)
